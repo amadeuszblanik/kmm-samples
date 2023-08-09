1df312b53d08f3a73f90793dd7fefe1eafef9f60
Amadeusz Blanik <amadeusz@blanik.me>
2023-08-09 19:12:39 +0200

    Added KMM-ViewModel and KMP-NativeCouritines

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -2,13 +2,14 @@ plugins {
     kotlin("multiplatform")
     kotlin("native.cocoapods")
     id("com.android.library")
+    id("com.google.devtools.ksp") version "1.9.0-1.0.11"
+    id("com.rickclephas.kmp.nativecoroutines") version "1.0.0-ALPHA-13"
+    kotlin("plugin.serialization") version "1.7.20"
 }
 
 @OptIn(org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi::class)
 kotlin {
-    targetHierarchy.default()
-
-    android {
+    androidTarget {
         compilations.all {
             kotlinOptions {
                 jvmTarget = "1.8"
@@ -29,17 +30,55 @@ kotlin {
             baseName = "shared"
         }
     }
-    
+
     sourceSets {
+        val multiplatformSettingsVersion = "1.0.0"
+        val kmmViewModelVersion = "1.0.0-ALPHA-12"
+
+        all {
+            languageSettings.optIn("kotlin.experimental.ExperimentalObjCName")
+        }
+
         val commonMain by getting {
             dependencies {
-                //put your multiplatform dependencies here
+                implementation("com.russhwolf:multiplatform-settings-no-arg:$multiplatformSettingsVersion")
+                implementation("com.russhwolf:multiplatform-settings-serialization:$multiplatformSettingsVersion")
+                implementation("com.russhwolf:multiplatform-settings-coroutines:$multiplatformSettingsVersion")
+                implementation("com.rickclephas.kmm:kmm-viewmodel-core:$kmmViewModelVersion")
             }
         }
+
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test"))
             }
+        }
+
+        val androidMain by getting {
+            dependencies {
+            }
+        }
+        val androidUnitTest by getting
+        val iosX64Main by getting
+        val iosArm64Main by getting
+        val iosSimulatorArm64Main by getting
+        val iosMain by creating {
+            dependsOn(commonMain)
+            iosX64Main.dependsOn(this)
+            iosArm64Main.dependsOn(this)
+            iosSimulatorArm64Main.dependsOn(this)
+
+            dependencies {
+            }
+        }
+        val iosX64Test by getting
+        val iosArm64Test by getting
+        val iosSimulatorArm64Test by getting
+        val iosTest by creating {
+            dependsOn(commonTest)
+            iosX64Test.dependsOn(this)
+            iosArm64Test.dependsOn(this)
+            iosSimulatorArm64Test.dependsOn(this)
         }
     }
 }
