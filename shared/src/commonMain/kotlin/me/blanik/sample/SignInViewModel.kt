0bd0bc8e215361c4d0740e423a6afc158d9e4eed
Amadeusz Blanik <amadeusz@blanik.me>
2023-08-11 01:23:07 +0200

    Added missing native couritines state notations

--- shared/src/commonMain/kotlin/me/blanik/sample/SignInViewModel.kt
+++ shared/src/commonMain/kotlin/me/blanik/sample/SignInViewModel.kt
@@ -1,20 +1,26 @@
 package me.blanik.sample
 
 import com.rickclephas.kmm.viewmodel.*
+import com.rickclephas.kmp.nativecoroutines.NativeCoroutinesState
 import kotlinx.coroutines.flow.*
 
 open class SignInViewModel: KMMViewModel() {
     private val _email = MutableStateFlow(viewModelScope, "")
     private val _password = MutableStateFlow(viewModelScope, "")
 
+    @NativeCoroutinesState
     val email = _email.asStateFlow()
+
+    @NativeCoroutinesState
     val password = _password.asStateFlow()
 
     fun setEmail(email: String) {
+        println("Set email: $email")
         _email.value = email
     }
 
     fun setPassword(password: String) {
+        println("Set password: $password")
         _password.value = password
     }
 }
