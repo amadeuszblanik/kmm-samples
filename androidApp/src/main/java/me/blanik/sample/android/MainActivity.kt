90312ddbba5b29ae11b817f4f29050962d35db27
Amadeusz Blanik <amadeusz@blanik.me>
2023-08-11 01:25:55 +0200

    Added Android implementation

--- androidApp/src/main/java/me/blanik/sample/android/MainActivity.kt
+++ androidApp/src/main/java/me/blanik/sample/android/MainActivity.kt
@@ -3,23 +3,35 @@ package me.blanik.sample.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
-import me.blanik.sample.Greeting
+import me.blanik.sample.SignInViewModel
 
 class MainActivity : ComponentActivity() {
+    private val viewModel: SignInViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             MyApplicationTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    GreetingView(Greeting().greet())
+                    Column {
+                        SignInScreen(viewModel)
+                    }
                 }
             }
         }
@@ -27,14 +39,36 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingView(text: String) {
-    Text(text = text)
+fun SignInScreen(
+    signInViewModel: SignInViewModel = SignInViewModel()
+) {
+    val emailState by signInViewModel.email.collectAsState()
+    val passwordState by signInViewModel.password.collectAsState()
+
+    Column {
+        Text("Input")
+        OutlinedTextField(
+            label = { Text(text = "Email") },
+            value = emailState,
+            onValueChange = signInViewModel::setEmail
+        )
+        OutlinedTextField(
+            label = { Text(text = "Password") },
+            value = passwordState,
+            onValueChange = signInViewModel::setPassword,
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+        )
+        Text("Output")
+        Text(text = emailState)
+        Text(text = passwordState)
+    }
 }
 
 @Preview
 @Composable
 fun DefaultPreview() {
     MyApplicationTheme {
-        GreetingView("Hello, Android!")
+        SignInScreen(signInViewModel = SignInViewModel())
     }
 }
